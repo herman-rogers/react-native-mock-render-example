4813b988b776525da42972694db913ebf5b5fedd
Herman Rogers <herman.rogers@gmail.com>
2018-05-07 13:49:12 -0400

    Update error console to output relevant messages

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # react-native-mock-render-example
+
+This package shows how to integrate react-native-mock into your React Native app.
+
+To get started just run ```yarn``` and ```yarn:test``` - if the tests pass you're good to go.
--- jest/setup.js
+++ jest/setup.js
@@ -24,8 +24,14 @@ const Adapter = require('enzyme-adapter-react-16');
 Enzyme.configure({ adapter: new Adapter() });
 
 // Ignore React Web errors when using React Native
-console.error = (message) => {
-  return message;
-};
+// allow other errors to propagate if they're relevant
+const suppressedErrors = /(React does not recognize the.*prop on a DOM element|Unknown event handler property|is using uppercase HTML|Received `true` for a non-boolean attribute `accessible`|The tag.*is unrecognized in this browser)/
+const realConsoleError = console.error
+console.error = message => {
+  if (message.match(suppressedErrors)) {
+    return
+  }
+  realConsoleError(message)
+}
 
 require('react-native-mock-render/mock');
